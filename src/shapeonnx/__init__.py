9e99e60b382db416657ce0bc08ad9f34f90b5e19
ZhongkuiMa <zhongkuima0419@gmail.com>
2026-05-12 20:00:19 +1000

    chore: bump version to 2026.5.1, enable check_untyped_defs, raise ruff complexity to 20, remove CI schedule

--- src/shapeonnx/__init__.py
+++ src/shapeonnx/__init__.py
@@ -1,6 +1,6 @@
 """Core shape inference module."""
 
-__version__ = "2026.5.0"
+__version__ = "2026.5.1"
 
 __docformat__ = "restructuredtext"
 __all__ = ["__version__", "extract_io_shapes", "infer_onnx_shape"]
